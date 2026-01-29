9bbadea27dafe0ce7a542cbf05eada5b455a76cd
Tyson Biegler <32854569+TysonBiegler@users.noreply.github.com>
2026-01-29 14:07:10 -0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,8 @@
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
 //   plugins: [react()],
-//   base: "/your-repo-name/", // Replace with your actual GitHub repo name
-// });
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/bathroom/", // 👈 repo name, with slashes
+  base: "/bathroom-code-tracker/",
 });
