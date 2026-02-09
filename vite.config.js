9ebef3b84639224f7141fdc476eb0d092e3ede88
Tyson Biegler <32854569+TysonBiegler@users.noreply.github.com>
2026-02-09 10:07:23 -0800

    adding PWA icons and manifest

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/bathroom-code-tracker/",
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
 });
